09e170d6a053dcbf330132e8799aab9646382232
yonggyo1125 <yonggyo00@kakao.com>
2023-07-25 21:52:38 +0900

    수업 소스

--- 4.Servlet & JSP/web_study/day01_2/src/main/java/servlets/JoinServlet.java
+++ 4.Servlet & JSP/web_study/day01_2/src/main/java/servlets/JoinServlet.java
@@ -11,6 +11,9 @@ import java.io.PrintWriter;
 public class JoinServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        //resp.setCharacterEncoding("UTF-8");
+
         PrintWriter out = resp.getWriter();
         out.print("<form method='post' action='join'>");
         out.print("아이디 : <input type='text' name='userId'><br>");
@@ -19,5 +22,12 @@ public class JoinServlet extends HttpServlet {
         out.print("회원명 : <input type='text' name='userNm'><br>");
         out.print("<button type='submit'>가입하기</button>");
         out.print("</form>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String userId = req.getParameter("userId");
+       String userPw = req.getParameter("userPw");
+       System.out.printf("userId=%s, userPw=%s%n", userId, userPw);
     }
 }
