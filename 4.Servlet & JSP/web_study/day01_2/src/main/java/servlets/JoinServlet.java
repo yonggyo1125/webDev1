e4029836f046dac76bd8dfab77dd0d68c86b7166
yonggyo1125 <yonggyo00@kakao.com>
2023-07-25 21:40:18 +0900

    수업 소스

--- 4.Servlet & JSP/web_study/day01_2/src/main/java/servlets/JoinServlet.java
+++ 4.Servlet & JSP/web_study/day01_2/src/main/java/servlets/JoinServlet.java
@@ -6,10 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JoinServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        PrintWriter out = resp.getWriter();
+        out.print("<form method='post' action='join'>");
+        out.print("아이디 : <input type='text' name='userId'><br>");
+        out.print("비밀번호 : <input type='password' name='userPw'><br>");
+        out.print("</form>");
     }
 }
