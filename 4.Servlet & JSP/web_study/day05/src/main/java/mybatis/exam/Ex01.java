b8400a969f2eead02b80e1428a9dd19a434d2038
yonggyo1125 <yonggyo00@kakao.com>
2023-07-31 21:08:09 +0900

    수업 소스

--- 4.Servlet & JSP/web_study/day05/src/main/java/mybatis/exam/Ex01.java
+++ 4.Servlet & JSP/web_study/day05/src/main/java/mybatis/exam/Ex01.java
@@ -1,10 +1,18 @@
 package mybatis.exam;
 
+import models.Users;
 import mybatis.MyConnection;
 import org.apache.ibatis.session.SqlSession;
 
 public class Ex01 {
     public static void main(String[] args) {
         SqlSession sqlSession = MyConnection.getSqlSession();
+        Users user = new Users();
+        user.setUserId("user01");
+        user.setUserPw("12345678");
+        user.setUserNm("사용자01");
+        user.setEmail("user01@test.org");
+        user.setMobile("01000000000");
+        int affectedRows = sqlSession.insert("insertUser", user);
     }
 }
