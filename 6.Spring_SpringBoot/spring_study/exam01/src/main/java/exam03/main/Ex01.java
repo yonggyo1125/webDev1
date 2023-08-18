05aef5a19a8bdd9537098ddba6832bc839095184
yonggyo1125 <yonggyo00@kakao.com>
2023-08-18 20:50:18 +0900

    수업 소스

---
+++ 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam03/main/Ex01.java
@@ -0,0 +1,27 @@
+package exam03.main;
+
+import exam03.config.*;
+import exam03.models.member.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalDateTime;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+        JoinService joinService = ctx.getBean("joinService", JoinService.class);
+        ListService listService = ctx.getBean("listService", ListService.class);
+
+        Member member = new Member();
+        member.setUserId("user01");
+        member.setUserPw("123456");
+        member.setUserNm("사용자01");
+        member.setRegDt(LocalDateTime.now());
+
+        joinService.join(member);
+        listService.print();
+
+        ctx.close();
+    }
+}
