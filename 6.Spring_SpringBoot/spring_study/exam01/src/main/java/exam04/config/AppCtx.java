2ade367beb8208b385b3984a54d36f779ca7ab0c
yonggyo1125 <yonggyo00@kakao.com>
2023-08-21 20:30:05 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam04/config/AppCtx.java
+++ 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam04/config/AppCtx.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import exam04.models.member.*;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "exam04.models")
+@ComponentScan(basePackages = "exam04.models",
+    excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes= ManualBean.class))
 public class AppCtx {
     /**
     @Bean
