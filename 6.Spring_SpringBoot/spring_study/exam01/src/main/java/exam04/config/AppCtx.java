b0ac8c8f00976f8e196ed2c16d29bef894d8fd71
yonggyo1125 <yonggyo00@kakao.com>
2023-08-21 20:19:18 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam04/config/AppCtx.java
+++ 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam04/config/AppCtx.java
@@ -9,9 +9,11 @@ import exam04.models.member.*;
 @Configuration
 @ComponentScan(basePackages = "exam04.models")
 public class AppCtx {
+    /**
     @Bean
     public MemberDao memberDao() {
         System.out.println("수동 등록 빈!");
         return new MemberDao();
     }
+    */
 }
