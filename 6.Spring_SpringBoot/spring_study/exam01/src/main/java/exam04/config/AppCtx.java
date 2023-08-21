41232046568456e33a4df652c1e11c7d77792d3f
yonggyo1125 <yonggyo00@kakao.com>
2023-08-21 20:51:43 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam04/config/AppCtx.java
+++ 6.Spring_SpringBoot/spring_study/exam01/src/main/java/exam04/config/AppCtx.java
@@ -11,8 +11,12 @@ import org.springframework.context.annotation.FilterType;
 /*
 @ComponentScan(basePackages = "exam04.models",
     excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes= ManualBean.class)) */
+/*
 @ComponentScan(basePackages = "exam04.models",
     excludeFilters = @ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, classes=MemberDao.class))
+ */
+@ComponentScan(basePackages = "exam04.models",
+    excludeFilters = @ComponentScan.Filter(type=FilterType.ASPECTJ, pattern="exam04.models..*Dao"))
 public class AppCtx {
     /**
     @Bean
