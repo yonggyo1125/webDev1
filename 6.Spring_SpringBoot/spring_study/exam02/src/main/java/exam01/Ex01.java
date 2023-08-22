c4fd8176007e614bd094483db9fec388ef5e2a6e
yonggyo1125 <yonggyo00@kakao.com>
2023-08-22 19:53:17 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam02/src/main/java/exam01/Ex01.java
+++ 6.Spring_SpringBoot/spring_study/exam02/src/main/java/exam01/Ex01.java
@@ -3,12 +3,12 @@ package exam01;
 public class Ex01 {
     public static void main(String[] args) {
 
-        ImplCalculator cal1 = new ImplCalculator();
-        long result1 = cal1.factorial(30);
+        Calculator cal1 = new ProxyCalculator(new ImplCalculator());
+        long result1 = cal1.factorial(10);
         System.out.printf("cal1=%d%n", result1);
 
-        RecCalculator cal2 = new RecCalculator();
-        long result2 = cal2.factorial(30);
+        Calculator cal2 = new ProxyCalculator(new RecCalculator());
+        long result2 = cal2.factorial(10);
         System.out.printf("cal2=%d%n", result2);
 
     }
