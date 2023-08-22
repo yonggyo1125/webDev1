27067d945bf281ee249507f8e5344af5794068bf
yonggyo1125 <yonggyo00@kakao.com>
2023-08-22 19:22:29 +0900

    수업 소스

---
+++ 6.Spring_SpringBoot/spring_study/exam02/src/main/java/exam01/Ex01.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        ImplCalculator cal1 = new ImplCalculator();
+        long result1 = cal1.factorial(30);
+        System.out.printf("cal1=%d%n", result1);
+
+        RecCalculator cal2 = new RecCalculator();
+        long result2 = cal2.factorial(30);
+        System.out.printf("cal2=%d%n", result2);
+
+    }
+}
--- 6.Spring_SpringBoot/spring_study/exam02/src/main/java/exam01/ImplCalculator.java
+++ 6.Spring_SpringBoot/spring_study/exam02/src/main/java/exam01/ImplCalculator.java
@@ -3,12 +3,18 @@ package exam01;
 public class ImplCalculator implements Calculator {
     @Override
     public long factorial(long num) {
-        long total = 1L;
+        long stime = System.nanoTime();
+        try {
+            long total = 1L;
 
-        for (long i = 1L; i <= num; i++) {
-            total *= i;
+            for (long i = 1L; i <= num; i++) {
+                total *= i;
+            }
+
+            return total;
+        } finally {
+            long etime = System.nanoTime();
+            System.out.printf("걸린 시간 : %d%n", etime - stime);
         }
-
-        return total;
     }
 }
