27c4bce1093da4d7d52665d18f80be928b5f3896
yonggyo1125 <yonggyo00@kakao.com>
2023-08-29 19:31:14 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/JoinValidator.java
+++ 6.Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/JoinValidator.java
@@ -32,6 +32,7 @@ public class JoinValidator implements Validator {
         String userId = joinForm.getUserId();
         String userPw = joinForm.getUserPw();
         String userPwRe = joinForm.getUserPwRe();
+        String mobile = joinForm.getMobile();
 
          // 1. 아이디 중복 여부 체크
         if (memberDao.exists(userId)) {
@@ -42,6 +43,25 @@ public class JoinValidator implements Validator {
         if (userPw != null && !userPw.isBlank() && userPwRe != null && !userPwRe.isBlank()
             && !userPw.equals(userPwRe)) {
             errors.rejectValue("userPwRe", "mismatch");
+        }
+
+        // 3. 휴대전화번호(필수 X) -> 입력된 경우는 형식 체크
+        if (mobile != null && !mobile.isBlank()) {
+            /**
+             * 1) 형식 통일화 010-0000-0000 010.0000.0000 -> 숫자로 변환
+             * 2) 패턴
+             *      010,016,011
+             *
+             *      000001000000000000000000
+             */
+
+            mobile = mobile.replaceAll("\\D", "");
+            joinForm.setMobile(mobile);
+
+            String pattern = "^01[016]\\d{3,4}\\d{4}$";
+            if (!mobile.matches(pattern)) { // 휴대전화 번호 형식이 아닌 경우
+                errors.rejectValue("mobile", "format");
+            }
         }
     }
 }
