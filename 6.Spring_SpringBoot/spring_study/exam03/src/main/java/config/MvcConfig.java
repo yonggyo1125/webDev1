8c1368022c349a0b8aa21822a2979d084f3a86c4
yonggyo1125 <yonggyo00@kakao.com>
2023-08-29 19:15:27 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam03/src/main/java/config/MvcConfig.java
+++ 6.Spring_SpringBoot/spring_study/exam03/src/main/java/config/MvcConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
+@Import(DbConfig.class)
 public class MvcConfig implements WebMvcConfigurer {
     /*
     @Autowired
