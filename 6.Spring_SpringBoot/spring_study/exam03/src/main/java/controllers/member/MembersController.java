83af2fdb9dc7d864db007c0bcc95c06597b28da4
yonggyo1125 <yonggyo00@kakao.com>
2023-08-30 19:32:42 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/MembersController.java
+++ 6.Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/MembersController.java
@@ -2,13 +2,15 @@ package controllers.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/members")
 public class MembersController {
     @GetMapping
-    public String list() {
+    public String list(@ModelAttribute SearchForm form) {
+
         return "member/list";
     }
 }
