e20ed21ad66af65d6dc08a639567d262f0406b7f
yonggyo1125 <yonggyo00@kakao.com>
2023-08-31 19:38:14 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam03/src/main/java/config/ControllerConfig.java
+++ 6.Spring_SpringBoot/spring_study/exam03/src/main/java/config/ControllerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"controllers", "models"})
+@ComponentScan({"controllers", "restcontrollers", "models"})
 public class ControllerConfig {
 
 }
--- 6.Spring_SpringBoot/spring_study/exam03/src/main/java/restcontrollers/MemberController.java
+++ 6.Spring_SpringBoot/spring_study/exam03/src/main/java/restcontrollers/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
@@ -19,5 +21,24 @@ public class MemberController {
         Member member = memberDao.get(userId);
 
         return member;
+    }
+
+    @GetMapping("/list")
+    public List<Member> list() {
+        List<Member> members = memberDao.getList();
+
+        return members;
+    }
+
+    /*
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hello!!";
+    }
+     */
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("Hello!!!!");
     }
 }
