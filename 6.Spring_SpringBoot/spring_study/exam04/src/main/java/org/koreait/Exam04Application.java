367bbba2117138a48258eb0a94d2627b1d1a01bb
yonggyo1125 <yonggyo00@kakao.com>
2023-09-04 21:53:13 +0900

    수업 소스

---
+++ 6.Spring_SpringBoot/spring_study/exam04/src/main/java/org/koreait/Exam04Application.java
@@ -0,0 +1,13 @@
+package org.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exam04Application.class, args);
+	}
+
+}
