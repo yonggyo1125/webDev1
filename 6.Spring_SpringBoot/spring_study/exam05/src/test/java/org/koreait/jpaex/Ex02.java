5c9f740a096464a076ac055e4a072970bfb6261a
yonggyo1125 <yonggyo00@kakao.com>
2023-09-05 21:51:47 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex02.java
+++ 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex02.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.koreait.entities.Member;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -32,4 +33,24 @@ public class Ex02 {
         em.flush();
     }
 
+    @Test
+    void test1() {
+        em.clear();
+        Member member = em.find(Member.class, 1L); // SELECT 문 수행 -> 엔티티 -> 영속 상태
+        System.out.println(member);
+
+        Member member2 = em.find(Member.class, 1L); // 영속성 컨텍스트의 엔티티 조회
+        System.out.println(member2);
+        System.out.println(member == member2);
+    }
+
+    @Test
+    void test2() {
+        Member member = em.find(Member.class, 1L);
+        member.setUserNm("(수정)사용자01");
+        //em.flush();
+
+        Member member2 = em.find(Member.class, 1L); // flush() 먼저 수행 -> 조회
+        System.out.println(member2);
+    }
 }
