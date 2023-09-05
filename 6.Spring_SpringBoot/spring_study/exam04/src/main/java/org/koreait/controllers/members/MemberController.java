43c4eeded3bda36c3fe5431476ad00283ce77ff3
yonggyo1125 <yonggyo00@kakao.com>
2023-09-05 19:11:35 +0900

    수업 소스

---
+++ 6.Spring_SpringBoot/spring_study/exam04/src/main/java/org/koreait/controllers/members/MemberController.java
@@ -0,0 +1,15 @@
+package org.koreait.controllers.members;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/join")
+    public void join() {
+
+    }
+}
