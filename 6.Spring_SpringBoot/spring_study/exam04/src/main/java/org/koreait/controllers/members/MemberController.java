72083c80236633db078c4c32ea9a2479da1f927e
yonggyo1125 <yonggyo00@kakao.com>
2023-09-05 19:21:26 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam04/src/main/java/org/koreait/controllers/members/MemberController.java
+++ 6.Spring_SpringBoot/spring_study/exam04/src/main/java/org/koreait/controllers/members/MemberController.java
@@ -1,15 +1,16 @@
 package org.koreait.controllers.members;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/join")
-    public void join() {
+    public String join() {
 
+        return "member/join";
     }
 }
