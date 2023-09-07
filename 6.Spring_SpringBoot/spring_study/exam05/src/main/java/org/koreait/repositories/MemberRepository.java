cc32b61aacd6bc097362969f7405ef884e21486f
yonggyo1125 <yonggyo00@kakao.com>
2023-09-07 20:12:57 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
+++ 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
@@ -3,6 +3,8 @@ package org.koreait.repositories;
 import org.koreait.entities.Member;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +16,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUserNmContainingOrderByUserIdDesc(String keyword);
 
     List<Member> findByUserNmContainingOrUserIdContainingOrderByUserIdDesc(String keyword1, String keyword2);
+
+    @Query("SELECT m FROM Member m WHERE m.userNm LIKE %:key1% OR m.userId LIKE %:key2% ORDER BY m.userId DESC")
+    List<Member> getList(@Param("key1") String keyword1, @Param("key2") String keyword2);
 }
