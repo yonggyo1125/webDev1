0781b1288d07f9f52f463065bb5d491deaa947ce
yonggyo1125 <yonggyo00@kakao.com>
2023-09-07 14:26:38 +0900

    수업 소스

---
+++ 6.Spring_SpringBoot/spring_study/day03/src/exam01/Ex01.java
@@ -0,0 +1,10 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+       int[] studentIds = new int[50];
+       for (int i = 0; i < 50; i++) {
+           studentIds[i] = i + 20231401;
+       }
+    }
+}
---
+++ 6.Spring_SpringBoot/spring_study/day03/src/exam01/Ex02.java
@@ -0,0 +1,21 @@
+package exam01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int[] nums = new int[4]; // 4개의 int 자료형 변수 생성
+        nums[0] = 10;
+        nums[1] = 20;
+        nums[2] = 30;
+        nums[3] = 40;
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(nums[i]);
+        }
+        /**
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+         */
+    }
+}
