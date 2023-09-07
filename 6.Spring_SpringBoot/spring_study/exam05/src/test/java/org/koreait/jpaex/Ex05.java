251e924a0cc77f6203ccbca04868ec25473ee372
yonggyo1125 <yonggyo00@kakao.com>
2023-09-07 21:23:16 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex05.java
+++ 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex05.java
@@ -3,11 +3,15 @@ package org.koreait.jpaex;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.koreait.constants.Role;
+import org.koreait.entities.BoardData;
 import org.koreait.entities.Member;
 import org.koreait.repositories.BoardDataRepository;
 import org.koreait.repositories.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -29,5 +33,18 @@ public class Ex05 {
                 .role(Role.USER)
                 .build();
 
+        memberRepository.saveAndFlush(member);
+
+        List<BoardData> items = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            BoardData item = BoardData.builder()
+                    .subject("제목" + i)
+                    .content("내용" + i)
+                    .member(member)
+                    .build();
+            items.add(item);
+        }
+
+        boardDataRepository.saveAllAndFlush(items);
     }
 }
