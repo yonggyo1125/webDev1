cd849b5eb70146369e87e65b19584c43b6ef7cc1
yonggyo1125 <yonggyo00@kakao.com>
2023-09-07 20:36:23 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
+++ 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
@@ -4,11 +4,13 @@ import org.koreait.entities.Member;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+
     Member findByUserId(String userId);
 
     List<Member> findByUserNmContaining(String keyword, Pageable pageable);
