5623283877d2929f5ae31cca32112740ee2907fb
yonggyo1125 <yonggyo00@kakao.com>
2023-09-08 19:48:43 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex07.java
+++ 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex07.java
@@ -1,10 +1,47 @@
 package org.koreait.jpaex;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.koreait.entities.BoardData;
+import org.koreait.entities.HashTag;
+import org.koreait.repositories.BoardDataRepository;
+import org.koreait.repositories.HashTagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class Ex07 {
 
+    @Autowired
+    private BoardDataRepository boardDataRepository;
+
+    @Autowired
+    private HashTagRepository hashTagRepository;
+
+    @BeforeEach
+    void init() {
+        List<HashTag> tags = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            HashTag tag = new HashTag();
+            tag.setTag("태그" + i);
+            tags.add(tag);
+        }
+
+        hashTagRepository.saveAllAndFlush(tags);
 
 
+        List<BoardData> items = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            BoardData item = BoardData.builder()
+                    .subject("제목" + i)
+                    .content("내용" + i)
+                    .tags(tags)
+                    .build();
+            items.add(item);
+        }
+
+        boardDataRepository.saveAllAndFlush(items);
+    }
 }
