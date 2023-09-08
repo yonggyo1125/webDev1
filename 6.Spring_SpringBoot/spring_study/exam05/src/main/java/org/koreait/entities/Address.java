bd03ae97af0e39739c90030f4b77558b9733a6fd
yonggyo1125 <yonggyo00@kakao.com>
2023-09-08 19:09:50 +0900

    수업 소스

---
+++ 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/Address.java
@@ -0,0 +1,25 @@
+package org.koreait.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length=10)
+    private String zonecode;
+
+    @Column(length=100)
+    private String address;
+
+    @Column(length=100)
+    private String addressSub;
+}
