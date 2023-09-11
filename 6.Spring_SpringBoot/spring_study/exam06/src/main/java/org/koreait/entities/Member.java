afa9ca2e2bd64f5b8dea09efd62928f0aecd5815
yonggyo1125 <yonggyo00@kakao.com>
2023-09-11 20:05:05 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
@@ -13,9 +13,18 @@ public class Member extends BaseEntity {
     @Id @GeneratedValue
     private Long userNo;
 
+    @Column(length=40, unique = true, nullable = false)
     private String userId;
+
+    @Column(length=65, nullable = false)
     private String userPw;
+
+    @Column(length=40, nullable = false)
     private String userNm;
+
+    @Column(length=100, nullable = false)
     private String email;
+
+    @Column(length=11)
     private String mobile;
 }
