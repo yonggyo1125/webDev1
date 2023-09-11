0a86d247eca076d28152059a5f8ed3db0ca16943
yonggyo1125 <yonggyo00@kakao.com>
2023-09-11 21:35:00 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/controllers/member/MemberController.java
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/controllers/member/MemberController.java
@@ -1,7 +1,10 @@
 package org.koreait.controllers.member;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,13 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     @GetMapping("/join")
-    public String join() {
+    public String join(@ModelAttribute JoinForm joinForm) {
 
         return "member/join";
     }
 
     @PostMapping("/join")
-    public String joinPs() {
+    public String joinPs(@Valid JoinForm joinForm, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "member/join";
+        }
 
         return "redirect:/member/login";
     }
