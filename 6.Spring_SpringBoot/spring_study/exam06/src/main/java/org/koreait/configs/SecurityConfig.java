9c1fe5242a44b617dd75fd0dc7880aa050cb1f0c
yonggyo1125 <yonggyo00@kakao.com>
2023-09-11 19:48:11 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.koreait.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,5 +13,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
@@ -0,0 +1,14 @@
+package org.koreait.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Member {
+
+}
