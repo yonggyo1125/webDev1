745d499614d825cce20b980e7067b4636035ec33
yonggyo1125 <yonggyo00@kakao.com>
2023-09-11 20:19:09 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/models/member/MemberInfo.java
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/models/member/MemberInfo.java
@@ -20,7 +20,7 @@ public class MemberInfo implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
