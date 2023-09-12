cfb91671debb47dfdba46c07f7f737a79da011f5
yonggyo1125 <yonggyo00@kakao.com>
2023-09-12 20:24:34 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -6,11 +6,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        /** 회원 인증 설정 - 로그인 */
+        http.formLogin(f -> {
+           f.loginPage("/member/login")
+                   .usernameParameter("userId")
+                   .passwordParameter("userPw")
+                   .successForwardUrl("/")
+                   .failureUrl("/member/login");
+
+        });
+
+        /** 회원 로그아웃 설정 */
+        http.logout(f -> {
+            f.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                    .logoutSuccessUrl("/member/login");
+        });
 
         return http.build();
     }
