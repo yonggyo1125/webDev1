cc84ae4bc7b7fb539f4529b7b715f65efcfd1cb1
yonggyo1125 <yonggyo00@kakao.com>
2023-09-13 19:33:55 +0900

    수업 소스

--- 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -1,14 +1,21 @@
 package org.koreait.configs;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.koreait.models.member.LoginFailureHandler;
 import org.koreait.models.member.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -39,6 +46,19 @@ public class SecurityConfig {
                     .anyRequest().permitAll(); // 나머지 페이지는 전체 접근 가능
         });
 
+        http.exceptionHandling(c -> {
+            c.authenticationEntryPoint((req, res,  e) -> {
+                String URI = req.getRequestURI();
+                if (URI.indexOf("/admin") != -1) { // 관리자 페이지 
+                    res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "NOT AUTHORIZED");
+                } else { // 회원전용 페이지
+                   // 로그인 페이지 이동
+                   res.sendRedirect(req.getContextPath() + "/member/login");
+
+                }
+
+            });
+        });
 
         return http.build();
     }
