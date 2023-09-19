c00d6b57cf341e4d44999f5247b7a8357dd6391a
yonggyo1125 <yonggyo00@kakao.com>
2023-09-19 20:10:33 +0900

    수업 소스

--- etc/TDD/Exam01/build.gradle.kts
+++ etc/TDD/Exam01/build.gradle.kts
@@ -13,8 +13,7 @@ repositories {
 dependencies {
     testImplementation("org.projectlombok:lombok:1.18.28")
     compileOnly("org.projectlombok:lombok:1.18.28")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.mockito:mockito-core:5.5.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.5.0")
     annotationProcessor("org.projectlombok:lombok")
